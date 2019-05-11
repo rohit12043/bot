32fa1585c9cea8110f1c57514e7955b2e72180d9
rohit12043 <rohitsinha12043@gmail.com>
2019-05-11 19:02:54 +0530

    spam

--- userbot/modules/spammer.py
+++ userbot/modules/spammer.py
@@ -1,3 +1,10 @@
+
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.b (the "License");
+# you may not use this file except in compliance with the License.
+#
+
 from asyncio import wait
 
 from userbot import LOGGER_GROUP, LOGGER, HELPER
@@ -30,10 +37,9 @@ async def bigspam(e):
         message = e.text
         counter = int(message[9:13])
         spam_message = str(e.text[13:])
-
-        if range(1, counter):
-            await e.respond(spam_message)
-
+        await wait(
+            [e.respond(spam_message) for i in range(counter)]
+            )
         await e.delete()
         if LOGGER:
             await e.client.send_message(
